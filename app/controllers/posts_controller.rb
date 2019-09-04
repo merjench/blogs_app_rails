0f569c4aae7ecc01fa9845f7733c9b6e5562fbaf
merjench <merjen.chary@gmail.com>
2019-09-04 00:51:29 -0400

    new/show/edit/index pages and functions implimented for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,53 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-  # @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post has been successfully updated"
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destoy
+    @post = Post.find(params[:id])
+    @post.destoy
+    flash[:notice] = "Post was deleted"
+    redirect_to posts_path
+  end
+
+
 
   def create
     @post = Post.new(post_params)
-    @post.user
-
+    if @post.save
+      flash[:notice] = "Your Post was created successfully"
+    redirect_to post_path(@post)
+  else
+    render 'new'
+  end
   end
 
+  private
 
-
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 
 end
